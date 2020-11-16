8770e3073235e898eddc7592c17cdf01e1d6594c
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-11-16 17:22:25 +0000

    Added User,Product and Order Models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
@@ -21,5 +22,7 @@ app.get("/api/products/:id", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} Mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} Mode on port ${PORT}`.yellow.bold
+  )
 );
