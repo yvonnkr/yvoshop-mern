dacf6ab1b31908fb42d5ebd8885bb2e1c059f5fb
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-11-19 14:58:21 +0000

    Create Product Controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,15 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-// @desc Fetch single product
-// @route GET /api/product/:id
-// @access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (!product) {
-      res.status(404);
-      throw new Error("Product not found");
-      //res.status(404).json({ message: "Product not found" });
-    }
-
-    res.json(product);
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
+
+// router.get("/", getProducts);
+// router.get("/:id", getProductById);
