39429d094e41188366dacc6fc344be375aa72c74
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-11-19 20:26:35 +0000

    User login reducer and action

--- frontend/src/reducers/combinedReducers.js
+++ frontend/src/reducers/combinedReducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import { productDetailsReducer, productListReducer } from "./productReducers";
 import { cartReducer } from "./cartReducers";
+import { userLoginReducer } from "./userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 export default reducer;
