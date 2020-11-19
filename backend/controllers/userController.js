cf1bb113bed8aa4a1afa197531f4b9b6e966a3f0
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-11-19 19:01:32 +0000

    Authentication middleware & user profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,4 +26,24 @@ const authUser = asyncHandler(async (req, res) => {
   });
 });
 
-export { authUser };
+// @desc Get user profile
+// @route GE /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  const { _id, name, email, isAdmin } = user;
+  res.json({
+    _id,
+    name,
+    email,
+    isAdmin,
+  });
+});
+
+export { authUser, getUserProfile };
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+export const requireAuth = asyncHandler(async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    try {
+      token = authHeader.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, invalid token");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+});
