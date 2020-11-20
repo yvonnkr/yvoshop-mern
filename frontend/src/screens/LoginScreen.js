c421fffe85172357ffc7a8b4f43c3b80d89ed8cd
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-11-20 00:21:20 +0000

    LoginScreen and User login functionality

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { login } from "../actions/userActions";
+import FormContainer from "../components/FormContainer";
+
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const dispatch = useDispatch();
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    // redirect if already logged-in
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  const renderForm = () => (
+    <Form onSubmit={submitHandler}>
+      <Form.Group controlId="email">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
+      <Button type="submit" variant="dark">
+        Sign In
+      </Button>
+    </Form>
+  );
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
+
+      {renderForm()}
+
+      <Row className="py-3">
+        <Col>
+          <h4>
+            New Customer?{" "}
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : "/register"}
+              className="all-links"
+            >
+              Register
+            </Link>
+          </h4>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
